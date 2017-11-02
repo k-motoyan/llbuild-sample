c4261c227c067ecbb873ae9c65e65e8022380550
kenta.motoyanagi <kenta.motoyanagi@glpgs.com>
2017-11-02 12:06:33 +0900

    Initial Commit

---
+++ llbuild-sample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  llbuild-sample
+//
+//  Created by 本柳賢太 on 2017/11/02.
+//  Copyright © 2017年 k-motoyan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
